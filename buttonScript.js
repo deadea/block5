040f900c4d98f2e6c79678c0839958b407a15339
Medeya <dea.2412@gmail.com>
2022-01-03 21:40:40 +0300

    swiper+button js

---
+++ buttonScript.js
@@ -0,0 +1,42 @@
+// переменные для показа скрытия по кнопке 
+let buttonOpen = document.querySelector('.button-open');
+let buttonClose = document.querySelector('.button-close');
+
+let hiddenOnTablet = document.querySelectorAll('.swiper-slide--hidden--tablet');
+let hiddenOnDesktop = document.querySelectorAll('.swiper-slide--hidden--desktop');
+
+ 
+
+ //событие по нажатию на кнопку показать 
+ buttonOpen.addEventListener('click', function(evt) {
+    evt.preventDefault();
+    buttonOpen.classList.toggle('button-open--hidden');
+ for (let i = 0; i < hiddenOnTablet.length; i++) {
+ hiddenOnTablet[i].classList.toggle('swiper-slide--hidden--tablet');
+ }
+ for (let i = 0; i < hiddenOnDesktop.length; i++) {
+   hiddenOnDesktop[i].classList.toggle('swiper-slide--hidden--desktop');
+}
+ buttonClose.classList.toggle('button-close--hidden');
+ });
+ 
+//событие по нажатию на кнопку скрыть 
+buttonClose.addEventListener('click', function(evt) {
+   evt.preventDefault();
+   buttonClose.classList.toggle('button-close--hidden');
+   for (let i = 0; i < hiddenOnTablet.length; i++) {
+      hiddenOnTablet[i].classList.toggle('swiper-slide--hidden--tablet');
+      }
+      for (let i = 0; i < hiddenOnDesktop.length; i++) {
+        hiddenOnDesktop[i].classList.toggle('swiper-slide--hidden--desktop');
+     }
+buttonOpen.classList.toggle('button-open--hidden');
+});
+
+
+
+ 
+// swiper-slide--hidden--desktop
+// swiper-slide--hidden--tablet
+ //button-open--hidden
+// button-close--hidden
